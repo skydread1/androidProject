0f041c7324411376deb03571b0d77bccdf1aca0d
nora <s543411@htw-berlin.de>
2018-10-10 15:54:25 +0200

    json lala

--- app/src/main/java/com/cpe/chat/UserLoginTask.java
+++ app/src/main/java/com/cpe/chat/UserLoginTask.java
@@ -1,12 +1,21 @@
 package com.cpe.chat;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+    final static private String TAG = UserLoginTask.class.getSimpleName();
 
     private final String mEmail;
     private final String mPassword;
@@ -26,7 +35,36 @@ public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
             URL url = new URL(myUrl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             responseCode = urlConnection.getResponseCode();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String input;
+            System.out.println("HELLO");
+            Log.i(TAG, "HELLO");
+            System.out.println("\n");
+            while ((input = in.readLine()) != null) {
+
+                JSONObject loginStat  = new JSONObject(input);
+
+                JSONArray cast = loginStat.getJSONArray("status");
+                for (int i=0; i<cast.length(); i++) {
+                    JSONObject s = cast.getJSONObject(i);
+                    String stat = s.getString("status");
+                    Log.i(TAG, "IN FOR");
+                    System.out.println("JSON");
+                    System.out.println("\n");
+                    Log.i(TAG, "status : " + stat);
+                    System.out.println("status : " + stat);
+                    System.out.println("\n");
+                }
+
+            }
+            in.close();
+
+            //JSONObject loginStatus = new JSONObject(responseCode);
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
