5d50a0491cb6f3fd7334cc70f2b140a5ba9e9ad0
nora <s543411@htw-berlin.de>
2018-10-27 17:22:18 +0200

    Chatwindow floating right

--- app/src/main/java/com/cpe/chat/MessageAdapter.java
+++ app/src/main/java/com/cpe/chat/MessageAdapter.java
@@ -6,9 +6,11 @@ import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,14 +25,18 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
         public TextView nameView;
-        //public TextView textView_left;
-        // public TextView textView_right;
+        public LinearLayout linearLayoutText;
+        public LinearLayout linearLayoutName;
+
 
 
         public ViewHolder(View v) {
             super(v);
             textView = v.findViewById(R.id.textView);
             nameView = v.findViewById(R.id.textView_Name_n_date);
+            linearLayoutText = v.findViewById(R.id.layout_txt);
+            linearLayoutName = v.findViewById(R.id.layout_name);
+
         }
     }
 
@@ -68,6 +74,9 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
             GradientDrawable gradientDrawable = (GradientDrawable) viewHolder.textView.getBackground().mutate();
             gradientDrawable.setColor(bubble_color);
 
+            viewHolder.linearLayoutText.setGravity(Gravity.RIGHT);
+            viewHolder.linearLayoutName.setGravity(Gravity.RIGHT);
+
             viewHolder.nameView.setText(messages.get(i).getSenderNickname()+" at " + messages.get(i).getDate());
             viewHolder.textView.setText(messages.get(i).getMessageContent());
         }else{
@@ -75,6 +84,9 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
             viewHolder.textView.setBackgroundResource(R.drawable.bubble_left);
             GradientDrawable gradientDrawable = (GradientDrawable) viewHolder.textView.getBackground().mutate();
             gradientDrawable.setColor(bubble_color);
+
+            viewHolder.linearLayoutText.setGravity(Gravity.LEFT);
+            viewHolder.linearLayoutName.setGravity(Gravity.LEFT);
 
             viewHolder.nameView.setText(messages.get(i).getSenderNickname()+" at " + messages.get(i).getDate());
             viewHolder.textView.setText(messages.get(i).getMessageContent());
